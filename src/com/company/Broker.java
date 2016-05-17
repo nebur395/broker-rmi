a24d373139ccdcbb290287baab8ca196f80d3690
Ruben Moreno Jimeno <nebur395@hotmail.com>
2016-05-17 12:21:52 +0200

    Add broker infrastructure(lol infraEStructure)

---
+++ src/com/company/Broker.java
@@ -0,0 +1,66 @@
+package com.company;
+
+/*
+ * AUTORES: Rubén Moreno Jimeno 680882 e Iñigo Gascón Royo 685215
+ * FICHERO: ServerB.java
+ * DESCRIPCIÓN:
+ */
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Broker implements BrokerInterface, Runnable {
+
+    private String ipRegistro; //IP del host del registro RMI
+
+    /**
+     *	Metodo constructor de la clase que asigna la IP de registro
+     */
+    public Broker(String ipRegistro) {
+        this.ipRegistro = ipRegistro;
+    }
+
+    /**
+     *
+     */
+    public void ejecuta(String server, String [] args) {
+        ;
+    }
+
+    /**
+     *
+     */
+    public void registraServer(String noseque) {
+
+    }
+    /**
+     *
+     */
+    public void registraServicio(String noseque) {
+
+    }
+    /**
+     *
+     */
+    public void listar_servicios(String noseque) {
+
+    }
+
+    public void run() {
+        try {
+            //Se crea un stub y posteriormente se introduce al registro
+            ServerAInterface stub = (ServerAInterface) UnicastRemoteObject.exportObject(this, 0);
+            Registry registry = LocateRegistry.getRegistry(ipRegistro);
+
+            registry.bind("BrokerInterface", stub);
+
+
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (AlreadyBoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
