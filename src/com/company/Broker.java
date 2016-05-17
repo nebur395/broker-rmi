e0f792ee5b978d3b98d24234a44f67f8fec63b68
Ruben Moreno Jimeno <nebur395@hotmail.com>
2016-05-17 17:51:48 +0200

    Add first iteration of 'ejecutar_servicio'

--- src/com/company/Broker.java
+++ src/com/company/Broker.java
@@ -7,6 +7,7 @@ package com.company;
  */
 
 import java.rmi.AlreadyBoundException;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -30,6 +31,41 @@ public class Broker implements BrokerInterface, Runnable {
      *
      */
     public String ejecutar_servicio(String nom_servicio, String[] parametros_servicio) {
+        boolean encontrado = false;
+        Iterator<Servidor> iterServidor = servidores.iterator();
+        Servidor servidor = null;
+        // Se itera en la lista de servidores con sus servicios, hasta encontrar el servicio
+        // [nom_servicio] y dejar en la variable [servidor] el servidor que contiene dicho servicio
+        while (iterServidor.hasNext() && !encontrado) {
+            servidor = iterServidor.next();
+            Iterator<Servicio> servicio = servidor.getServicios().iterator();
+            while (servicio.hasNext() && !encontrado) {
+                if (servicio.next().getNombre().equals(nom_servicio)) {
+                    encontrado = true;
+                }
+            }
+        }
+        if (encontrado) {
+            try {
+                // Se coge el objeto remoto del broker
+                Registry registry = LocateRegistry.getRegistry(servidor.getIP());
+                switch (servidor.getNombre()) {
+                    case "ServerAInterface":
+                        ServerAInterface server = (ServerAInterface) registry.lookup(servidor
+                                .getNombre());
+                        break;
+                    default:
+                        ServerBInterface server = (ServerBInterface) registry.lookup(servidor
+                                .getNombre());
+                }
+
+
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            } catch (NotBoundException e) {
+                e.printStackTrace();
+            }
+        }
         return "";
     }
 
@@ -58,7 +94,7 @@ public class Broker implements BrokerInterface, Runnable {
      */
     public ArrayList<String> listar_servicios(String noseque) {
         ArrayList<String> listado = new ArrayList<String>();
-        Iterator<Servidor> iterServer= servidores.iterator();
+        Iterator<Servidor> iterServer = servidores.iterator();
         while (iterServer.hasNext()) {
             Iterator<Servicio> iterServicio = iterServer.next().getServicios().iterator();
             while (iterServicio.hasNext()) {
